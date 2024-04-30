ff68e3ee59a700e002112fc5ba18e99410bb7ac7
Daniel Loli <nilton.loli@unmsm.edu.pe>
2024-04-30 15:41:14 -0500

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 import { BrowserRouter } from 'react-router-dom';
-import AppContext, { AppProvider } from './context';
 import './App.css';
 import AppRoutes from './AppRoutes';
 
 const App = () => {
   return (
-    <AppProvider>
       <BrowserRouter>
         <AppRoutes />
       </BrowserRouter>
-    </AppProvider>
   );
 };
 
--- src/components/CardList/Card.jsx
+++ src/components/CardList/Card.jsx
@@ -9,10 +9,10 @@ const Card = ({ data }) => {
         <span className='absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5'>{data?.category}</span>
         <img className='w-full h-full object-cover rounded-lg' src={data?.urlToImage} alt={data?.title} />
       </figure>
-      <p className='flex justify-between items-center gap-4 mb-2'>
+      <div className='flex justify-between items-center gap-4 mb-2'>
         <h2 className='text-md font-bold text-purple font-sans'>{data?.title}</h2>
         <span className='text-sm font-light border-l border-gray-600 pl-4 text-nowrap'>{data?.publishedAt.split("T",2)[0]}</span>
-      </p>
+      </div>
       <p className='font-serif text-sm'>{data?.description} <span className='text-blue-600 underline underline-offset-4'><NavLink to={`/articles/${data?.title}`}>
           Ver artículo completo
       </NavLink> </span></p>
